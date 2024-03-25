3825e8581c7415d64d13acb5d526be9c4baa013e
Max I <clickkava@gmail.com>
2024-03-25 10:52:38 +0200

    Add loader in the shop page

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -6,14 +6,20 @@ import ProductCard from "./components/ProductCard";
 const Shop = () => {
   const data = useData('https://fakestoreapi.com/products/category/jewelery');
   const [jeweleryData, setJeweleryData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (data) {
       setJeweleryData(data);
+      setIsLoading(false)
     }
   }, [data]);
 
   console.log(jeweleryData);
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
 
   return (
     <>
