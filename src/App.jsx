320f9ee0ac6e1eccbb6fc762e2c7963ccec81c18
Max I <clickkava@gmail.com>
2024-03-25 16:23:03 +0200

    Change routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+
 function App() {
   
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className="wrapper">
+      <Header />
+      <Outlet />
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,40 +3,23 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
 import Shop from './Shop.jsx'
-import Header from './components/Header.jsx';
 import Cart from './Cart.jsx';
 import './index.css'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: 
-    <>
-      <div className='wrapper'>
-        <Header />
-        <App />
-      </div>
-    </>,
-  },
-  {
-    path: "shop",
-    element: 
-    <>
-      <div className='wrapper'>
-        <Header />
-        <Shop />
-      </div>
-    </>,
-  },
-  {
-    path: "cart",
-    element: 
-    <>
-      <div className='wrapper'>
-        <Header />
-        <Cart />
-      </div>
-    </>,
+    element: <App />,
+    children: [
+      {
+        path: "shop",
+        element: <Shop />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+    ],
   },
 ]);
 
